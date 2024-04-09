416be533d9a9328e14b4736c71e7155edeca8671
DestroyerOfCleanCode <148398810+DestroyerOfCleanCode@users.noreply.github.com>
2024-04-08 17:46:46 -0700

    Update SQLiteJDBC.java

--- src/application/SQLiteJDBC.java
+++ src/application/SQLiteJDBC.java
@@ -2,6 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class SQLiteJDBC {
@@ -15,8 +16,38 @@ public class SQLiteJDBC {
            System.out.println("Opened database successfully");
 
            stmt = c.createStatement();
-           String sql = ""; 
+           String sql = "CREATE TABLE Account (\r\n"
+           		+ "    ID INTEGER PRIMARY KEY AUTOINCREMENT,\r\n"
+           		+ "    Username TEXT NOT NULL,\r\n"
+           		+ "    Password TEXT NOT NULL,\r\n"
+           		+ "    Type TEXT NOT NULL,\r\n"
+           		+ "    UserID INTEGER NOT NULL\r\n"
+           		+ ");"; 
            stmt.executeUpdate(sql);
+           
+           sql = "INSERT INTO Account(Username, Password, Type, UserID) VALUES ('testaccount1', 'password', 'patient', 1);";
+           stmt.executeUpdate(sql);
+           
+           sql = "INSERT INTO Account(Username, Password, Type, UserID) VALUES ('testaccount2', 'password', 'nurse', 2);";
+           stmt.executeUpdate(sql);
+           
+           sql = "INSERT INTO Account(Username, Password, Type, UserID) VALUES ('testaccount3', 'password', 'doctor', 3);";
+           stmt.executeUpdate(sql);
+           
+           ResultSet rs = stmt.executeQuery( "SELECT * FROM Account;" );
+           
+           while ( rs.next() ) {
+               int id = rs.getInt("ID");
+               String  username = rs.getString("Username");
+               String  password = rs.getString("Password");
+               String  type = rs.getString("Type");
+               int userID = rs.getInt("UserID");
+               
+               System.out.println(id + "," + username + "," + password + "," + type + "," + userID);
+            }
+           
+            rs.close();
+           
            stmt.close();
            
            c.close();
@@ -24,6 +55,5 @@ public class SQLiteJDBC {
            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
            System.exit(0);
         }
-        System.out.println("Table created successfully");
     }
 }
