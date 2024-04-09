ed7c95969344a11bb4529f22e072b29373c3a1a8
acwang123w <toothpik123w@gmail.com>
2024-04-08 20:26:08 -0700

    Initialized OfficeMenuController
    Added dependencies to corresponding fxml

--- src/application/OfficeMenuController.java
+++ src/application/OfficeMenuController.java
@@ -12,9 +12,28 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class OfficeMenuController {
+	@FXML
+	private Text greeting;
+		
+    @FXML
+    private void logout() throws IOException {
+        System.out.print("logout pressed");
+    	Main.id = 0;
+    	Main.setRoot("Login");
+    }
 	
     @FXML
     private void newPatient() throws IOException {
     	Main.setRoot("MakeNewPatientPage");
+    }
+    
+    @FXML
+    private void existingPatient() throws IOException {
+    	Main.setRoot("FindPatientPage");
+    }
+    
+    @FXML
+    private void viewMessages() throws IOException {
+    	Main.setRoot("OfficeMessagingPage");
     }
 }
