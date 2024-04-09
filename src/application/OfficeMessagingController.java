7e4032ec5d6ffb9e8842ea1714cef4e740d71a77
acwang123w <toothpik123w@gmail.com>
2024-04-08 21:42:01 -0700

    Initializes OfficeMessagingController.java
    Added dependencies to corresponding fxml file

--- src/application/OfficeMessagingController.java
+++ src/application/OfficeMessagingController.java
@@ -1,5 +1,28 @@
 package application;
 
-public class OfficeMessagingController {
+import java.io.IOException;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class OfficeMessagingController {
+	@FXML
+	private Text greeting;
+	
+	@FXML
+	private VBox messageVBox;
+	
+	@FXML
+    private void back() throws IOException {
+        System.out.print("back pressed");
+        Main.setRoot("OfficeMenuPage");
+    }
+	
+	@FXML
+    private void logout() throws IOException {
+        System.out.print("logout pressed");
+    	Main.id = 0;
+    	Main.setRoot("Login");
+    }
 }
