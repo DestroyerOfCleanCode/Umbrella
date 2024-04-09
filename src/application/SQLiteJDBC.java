fb98b449c577a13368113a98d8d6b1b241910995
DestroyerOfCleanCode <148398810+DestroyerOfCleanCode@users.noreply.github.com>
2024-04-08 17:39:34 -0700

    Update SQLiteJDBC.java

--- src/application/SQLiteJDBC.java
+++ src/application/SQLiteJDBC.java
@@ -2,18 +2,28 @@ package application;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class SQLiteJDBC {
     public static void main(String args[]) {
-        Connection c = null;
-
+    	Connection c = null;
+        Statement stmt = null;
+        
         try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
+           Class.forName("org.sqlite.JDBC");
+           c = DriverManager.getConnection("jdbc:sqlite:test.db");
+           System.out.println("Opened database successfully");
+
+           stmt = c.createStatement();
+           String sql = ""; 
+           stmt.executeUpdate(sql);
+           stmt.close();
+           
+           c.close();
+        } catch ( Exception e ) {
+           System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+           System.exit(0);
         }
-        System.out.println("Opened database successfully");
+        System.out.println("Table created successfully");
     }
 }
