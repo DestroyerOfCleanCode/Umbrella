c90159c93c109e255144676b08603d227134082c
acwang123w <toothpik123w@gmail.com>
2024-04-08 19:49:57 -0700

    Initialized ExistingPatientsPageController
    Added dependencies to the corresponding fxml

--- src/application/ExistingPatientsPageController.java
+++ src/application/ExistingPatientsPageController.java
@@ -1,5 +1,83 @@
 package application;
 
-public class ExistingPatientsPageController {
+import java.io.IOException;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class ExistingPatientsPageController {
+	@FXML
+	private Text greeting;
+	@FXML
+	private Text patientName;
+	
+	@FXML
+	private Text firstname;
+	@FXML
+	private Text lastname;
+	@FXML
+	private Text dob;
+	@FXML
+	private Text sex;
+	@FXML
+	private Text bloodpressure;
+	@FXML
+	private Text weight;
+	@FXML
+	private Text height;
+	
+	@FXML
+	private TextField phonenumber;
+	@FXML
+	private TextField email;
+	@FXML
+	private TextField address;
+	
+	@FXML
+	private TextField insurancenumber;
+	
+	@FXML
+	private TextField pharmaddress;
+	@FXML
+	private TextField pharmnumber;
+	
+	@FXML
+	private VBox healthHistoryBox;
+	
+	@FXML
+	private VBox immunizationsBox;
+	
+	@FXML
+	private VBox pastAppointmentsBox;
+	
+	@FXML
+	private VBox MessageBox;	
+	
+	@FXML
+	private TextField messageField;
+	
+	@FXML
+    private void logout() throws IOException {
+        Main.setRoot("Login");
+        Main.id = 0;
+    }
+    
+	@FXML
+    private void goback() throws IOException {
+        Main.setRoot("OfficeMenuPage");
+    }
+    
+    @FXML
+    private void message() throws IOException {
+    	String message = messageField.getText();
+    	System.out.print(message);
+    }
+    
+    @FXML
+    private void appointment() throws IOException {
+    	System.out.print("appointment button pressed");
+    }
+    
 }
