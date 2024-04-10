67d378490b6c9bacb14597328522da1d7db62d9e
DestroyerOfCleanCode <148398810+DestroyerOfCleanCode@users.noreply.github.com>
2024-04-09 21:42:39 -0700

    removed db tests

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,7 +7,6 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.fxml.FXMLLoader;
 
